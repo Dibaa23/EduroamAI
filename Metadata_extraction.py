d83121fc762ee54bb35dd86fed4f3f77101690c1
prajyaguru003 <141519166+prajyaguru003@users.noreply.github.com>
2024-10-26 00:16:12 -0400

    # This is a combination of 4 commits. # This is the 1st commit message:
    Add files via upload
    # This is the commit message #2:
    
    chatbot
    
    # This is the commit message #3:
    
    removed API key
    
    # This is the commit message #4:
    
    Add files via upload

---
+++ Metadata_extraction.py
@@ -0,0 +1,25 @@
+import os
+import yt_dlp
+
+
+def download_video(url, output_folder='downloads', output_path='%(title)s.%(ext)s'):
+    # Ensure output folder exists
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Set the full output path including the folder
+    full_output_path = os.path.join(output_folder, output_path)
+
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': full_output_path,
+        'merge_output_format': 'mp4',
+        'quiet': False,  # Set to True to suppress output
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        # Extract video information and download the video
+        info_dict = ydl.extract_info(url, download=True)
+        # Get the video title
+        video_title = info_dict.get('title', None)
+        return video_title
